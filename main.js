a0145b529f829670b8413c493ef6d837e226d339
Mary Froust <mary.froust84@icloud.com>
2025-03-18 14:59:56 -0500

    conditionally-simple

---
+++ main.js
@@ -0,0 +1,54 @@
+/******************************
+ * YOUR CODE GOES RIGHT HERE! *
+ ******************************/
+function isItLong(str){
+  if(str.length > 20){
+    return "That's a long string!"
+  }
+}
+function isItMedium(str){
+  if(str.length <=10 >=20){
+    return "That's a regular sized string!"
+  }else if(str.length ===10){
+    return "That's a regular sized string!"
+  }else if(str.length ===20){
+    return "That's a regular sized string!"
+  }
+}
+
+
+
+/**********************************
+ *      OUR CODE GOES BELOW.      *
+ * THIS IS FOR INTERNAL USE ONLY. *
+ * DO NOT ADD OR CHANGE ANYTHING! *
+ **********************************/
+
+if (typeof isItLong === 'undefined') {
+  isItLong = undefined;
+}
+
+if (typeof isItMedium === 'undefined') {
+  isItMedium = undefined;
+}
+
+if (typeof isItShort === 'undefined') {
+  isItShort = undefined;
+}
+
+if (typeof howLongIsMyString === 'undefined') {
+  howLongIsMyString = undefined;
+}
+
+if (typeof personsHeight === 'undefined') {
+  personsHeight = undefined;
+}
+
+
+module.exports = {
+  isItLong,
+  isItMedium,
+  isItShort,
+  howLongIsMyString,
+  personsHeight,
+}
